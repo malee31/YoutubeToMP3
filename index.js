2d7d50d056c1d8a99e05da390c7ce42160052a56
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-12-27 16:36:26 -0800

    Refactored code to look cleaner using multiple async functions

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const path = require("path");
 const prompt = require("prompt");
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
+const thumbnailDownload = require("./download.js");
 require("dotenv").config();
 
 prompt.message = "";
 prompt.start();
 
 async function start() {
-	return new Promise(async(resolve, reject) => {
+	return new Promise(async resolve => {
 		let {url} = await prompt.get({
 			properties: {
 				url: {
@@ -17,59 +18,95 @@ async function start() {
 				}
 			}
 		});
+
 		const info = await ytdl.getInfo(url);
 		url = info.videoDetails.video_url;
 		console.log(`Processed Youtube URL: ${url}`);
 
-		let outputFile = (await prompt.get("What File Name Would You Like to Give the Audio File?"))["What File Name Would You Like to Give the Audio File?"];
-		if(!outputFile.endsWith(".mp3")) outputFile += ".mp3";
-		console.log(`Audio will be saved in ${outputFile}`);
+		let metaData = (await prompt.get({
+			properties: {
+				fileName: {
+					description: "What File Name Would You Like to Give the Audio File?"
+				},
+				mp3Title: {
+					description: "What Would You Like to Title the File? (Leave Blank to Set File Name as Title)"
+				},
+
+			}
+		}));
+
+		if(!metaData.fileName.endsWith(".mp3")) metaData.fileName += ".mp3";
+		console.log(`Audio will be saved in ${metaData.fileName}`);
+		if(!metaData.mp3Title) metaData.mp3Title = metaData.fileName.slice(0, metaData.fileName.length - 4);
+		console.log(`Title will be set as ${metaData.mp3Title}`);
 
 		console.log("========== Download Started ==========");
 
-		// console.log(Object.keys(info.videoDetails));
+		let downloadedPath = await ytDownload(info, url);
+		downloadedPath = await convertToMp3(downloadedPath);
+		downloadedPath = await addMetaData(downloadedPath, metaData);
+		console.log(`Final Result Saved To: ${downloadedPath}`);
+		resolve();
+	});
+}
+
+async function ytDownload(info, url) {
+	return new Promise((resolve, reject) => {
 		let format = ytdl.chooseFormat(info.formats, {
 			quality: "highestaudio",
 			filter: "audioonly"
 		});
 
+		const ytDownloadPath = path.resolve(__dirname, `downloads/temp.${format.container}`);
 		let youtubeDownload = ytdl(url, {format: format});
-		youtubeDownload.on("finish", err => {
-			if(err) {
-				console.warn("Something went wrong while downloading video");
-				console.error(err);
-			}
-			console.log("Youtube Download Complete");
 
-			// Note: I might have on("end", err => {}) wrong. It might be on("error", err => {})
-			const ffmpegProcess = ffmpeg(path.resolve(__dirname, `downloads/temp.${format.container}`));
-			// const ffmpegProcess = ffmpeg(path.resolve(__dirname, "downloads/temp.webm"));
-			ffmpegProcess.format("mp3")
-				.on("end", err => {
-					if(err) {
-						console.log("Something went wrong while converting to MP3");
-						reject(err);
-					}
-					console.log('========== MP3 Conversion Finished ==========');
-					const renameProcess = ffmpeg(path.resolve(__dirname, `downloads/temp.mp3`));
-					renameProcess
-						.addOutputOptions('-i', path.resolve(__dirname, `downloads/cover.png`), '-map', '0:0', '-map', '1:0', '-c', 'copy', '-id3v2_version', '3')
-						.on("end", err => {
-							if(err) {
-								console.warn("Something went wrong while adding a cover image");
-								reject(err);
-							}
-							resolve();
-						})
-						.save(path.resolve(__dirname, `downloads/${outputFile}`));
-					// .save(path.resolve(__dirname, "downloads/finalTemp.mp3"));
-				}).save(path.resolve(__dirname, `downloads/temp.mp3`));
-
-			//ffmpegProcess.addOutputOption('-metadata', 'title="Song Name"');
+		youtubeDownload.on("error", err => {
+			console.warn("Something went wrong while downloading video");
+			reject(err);
 		});
+
+		youtubeDownload.on("finish", () => {
+			console.log("Youtube Download Complete");
+			resolve(ytDownloadPath);
+		});
+
 		// See https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/issues/417 for removing the need to write to file first
 		youtubeDownload
-			.pipe(fs.createWriteStream(path.resolve(__dirname, `downloads/temp.${format.container}`)));
+			.pipe(fs.createWriteStream(ytDownloadPath));
+	});
+}
+
+async function convertToMp3(filePath) {
+	return new Promise((resolve, reject) => {
+		const newPath = path.resolve(__dirname, `downloads/temp.mp3`);
+		const ffmpegProcess = ffmpeg(filePath);
+		ffmpegProcess.format("mp3")
+			.on("error", err => {
+				console.log("Something went wrong while converting to MP3");
+				reject(err);
+			})
+			.on("end", () => {
+				console.log('========== MP3 Conversion Finished ==========');
+				resolve(newPath);
+			}).save(newPath);
+		//ffmpegProcess.addOutputOption('-metadata', 'title="Mp3 Name"');
+	});
+}
+
+async function addMetaData(mp3Path, metaData) {
+	const saveTo = path.resolve(__dirname, `downloads/${metaData.fileName}`);
+	return new Promise((resolve, reject) => {
+		const renameProcess = ffmpeg(mp3Path);
+		renameProcess
+			.addOutputOptions('-i', path.resolve(__dirname, `downloads/cover.png`), '-map', '0:0', '-map', '1:0', '-c', 'copy', '-id3v2_version', '3')
+			.on("end", err => {
+				if(err) {
+					console.warn("Something went wrong while adding a cover image");
+					reject(err);
+				}
+				resolve(saveTo);
+			})
+			.save(saveTo);
 	});
 }
 
